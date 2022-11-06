8c92d98dbc33930cddc3047bed4b68bc539e0442
Ali <akbarsatriaxtkj2@gmail.com>
2022-11-07 00:27:19 +0700

    Backend berjalan

---
+++ config/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const controllers = require("../app/controllers");
+const cors = require('cors');
+const handleGoogleLoginOrRegister = require("../handleGoogleLogin");
+
+const apiRouter = express.Router();
+apiRouter.use(express.json(), cors());
+
+/**
+ * Authentication Resource
+ * */
+apiRouter.get(
+  "/api/v1/whoami",
+  controllers.api.v1.authController.authorize,
+  controllers.api.v1.authController.whoAmI
+);
+apiRouter.post("/api/v1/login", controllers.api.v1.authController.login);
+apiRouter.post("/api/v1/register", controllers.api.v1.authController.register);
+apiRouter.post("/v1/auth/google", handleGoogleLoginOrRegister);
+
+apiRouter.use(controllers.api.main.onLost);
+apiRouter.use(controllers.api.main.onError);
+
+module.exports = apiRouter;
